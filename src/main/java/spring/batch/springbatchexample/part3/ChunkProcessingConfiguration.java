b5992c2070dde0a34e83c51d1589f7b5cd253a16
wisdom46 <wisdom1163@gmail.com>
2022-01-26 23:29:26 +0900

    JobParameters 이해

--- src/main/java/spring/batch/springbatchexample/part3/ChunkProcessingConfiguration.java
+++ src/main/java/spring/batch/springbatchexample/part3/ChunkProcessingConfiguration.java
@@ -1,10 +1,13 @@
 package spring.batch.springbatchexample.part3;
 
+import io.micrometer.core.instrument.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -13,6 +16,7 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -36,14 +40,16 @@ public class ChunkProcessingConfiguration {
         return jobBuilderFactory.get("chunkProcessingJob")
                 .incrementer(new RunIdIncrementer())
                 .start(this.taskBaseStep())
-                .next(this.chunkBaseStep())
+                .next(this.chunkBaseStep(null)) // null로 실행해도 잘 됨. JobScope 어노테이션 때문에 가능
                 .build();
     }
 
     @Bean
-    public Step chunkBaseStep() {
+    @JobScope
+    public Step chunkBaseStep(@Value("#{jobParameters[chunkSize]}") String chunkSize) {
+
         return stepBuilderFactory.get("chunkBaseStep")
-                .<String, String>chunk(10)// 100개의 데이터를 10개씩 나눈다는 얘기. 5로 설정하면 20번씩
+                .<String, String>chunk(StringUtils.isNotEmpty(chunkSize) ? Integer.parseInt(chunkSize) : 10)// 100개의 데이터를 10개씩 나눈다는 얘기. 5로 설정하면 20번씩
                 .reader(itemReader())
                 .processor(itemProcessor())
                 .writer(itemWriter())
@@ -85,8 +91,10 @@ public class ChunkProcessingConfiguration {
         List<String> items = getItems();
         return (contribution, chunkContext) -> {
             StepExecution stepExecution = contribution.getStepExecution();
+            JobParameters jobParameters = stepExecution.getJobParameters();
 
-            int chunkSize = 10;
+            String value = jobParameters.getString("chunkSize", "10");
+            int chunkSize = StringUtils.isNotEmpty(value) ? Integer.parseInt(value) : 10;
             int fromIndex = stepExecution.getReadCount();
             int toIndex = fromIndex + chunkSize;
 
@@ -95,7 +103,7 @@ public class ChunkProcessingConfiguration {
             }
             List<String> subList = items.subList(fromIndex, toIndex);
 
-            log.info("task item size : {}", items.size());
+            log.info("task item size : {}", subList.size());
 
             stepExecution.setReadCount(toIndex);
 
