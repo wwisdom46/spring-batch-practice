5778bf15cf5b495a5e77abfe4f8a9477ceb590db
wisdom46 <wisdom1163@gmail.com>
2022-01-26 22:59:56 +0900

    Task 기반 배치와 Chunk 기반 배치

---
+++ src/main/java/spring/batch/springbatchexample/part3/ChunkProcessingConfiguration.java
@@ -0,0 +1,116 @@
+package spring.batch.springbatchexample.part3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@Slf4j
+public class ChunkProcessingConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public ChunkProcessingConfiguration(JobBuilderFactory jobBuilderFactory,
+                                        StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job chunkProcessingJob() {
+        return jobBuilderFactory.get("chunkProcessingJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.taskBaseStep())
+                .next(this.chunkBaseStep())
+                .build();
+    }
+
+    @Bean
+    public Step chunkBaseStep() {
+        return stepBuilderFactory.get("chunkBaseStep")
+                .<String, String>chunk(10)// 100개의 데이터를 10개씩 나눈다는 얘기. 5로 설정하면 20번씩
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemWriter<String> itemWriter() {
+        return items -> log.info("chunk item size : {}", items.size());
+        //return items -> items.forEach(log::info);
+    }
+
+    /**
+     * reader에서 생성한 데이터를 가공하거나 writer로 넘길지말지 결정
+     * itemProcessor에서 null로 넘기 해당 item은 writer로 넘어갈 수 없게 됨
+     */
+    private ItemProcessor<String, String> itemProcessor() {
+        return item -> item + ", Spring Batch";
+    }
+
+    private ItemReader<String> itemReader() {
+        return  new ListItemReader<>(getItems());
+    }
+
+    @Bean
+    public Step taskBaseStep() {
+        return stepBuilderFactory.get("taskBaseStep")
+                .tasklet(this.tasklet())
+                .build();
+    }
+
+    private Tasklet tasklet() {
+//        return (contribution, chunkContext) -> {
+//            List<String> items = getItems();
+//            log.info("task item size : {}", items.size());
+//
+//            return RepeatStatus.FINISHED;
+//        };
+        // tasklet도 chunk처럼 쪼개보자
+        List<String> items = getItems();
+        return (contribution, chunkContext) -> {
+            StepExecution stepExecution = contribution.getStepExecution();
+
+            int chunkSize = 10;
+            int fromIndex = stepExecution.getReadCount();
+            int toIndex = fromIndex + chunkSize;
+
+            if (fromIndex >= items.size()) {
+                return  RepeatStatus.FINISHED;
+            }
+            List<String> subList = items.subList(fromIndex, toIndex);
+
+            log.info("task item size : {}", items.size());
+
+            stepExecution.setReadCount(toIndex);
+
+            return RepeatStatus.CONTINUABLE; // 이 tasklet을 반복해서 실행하라는 의미
+        };
+    }
+
+    private List<String> getItems() {
+        List<String> items = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            items.add(i + " Hello");
+        }
+        return items;
+    }
+
+
+}
