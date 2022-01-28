6a2306ad1e526028edcc0682de7bfc59b2fe4682
wisdom46 <wisdom1163@gmail.com>
2022-01-28 23:35:50 +0900

    JobScope, StepScope 이해

--- src/main/java/spring/batch/springbatchexample/part3/ChunkProcessingConfiguration.java
+++ src/main/java/spring/batch/springbatchexample/part3/ChunkProcessingConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
@@ -76,11 +77,13 @@ public class ChunkProcessingConfiguration {
     @Bean
     public Step taskBaseStep() {
         return stepBuilderFactory.get("taskBaseStep")
-                .tasklet(this.tasklet())
+                .tasklet(this.tasklet(null))
                 .build();
     }
 
-    private Tasklet tasklet() {
+    @Bean
+    @StepScope
+    public Tasklet tasklet(@Value("#{jobParameters[chunkSize]}") String value) {
 //        return (contribution, chunkContext) -> {
 //            List<String> items = getItems();
 //            log.info("task item size : {}", items.size());
@@ -91,9 +94,9 @@ public class ChunkProcessingConfiguration {
         List<String> items = getItems();
         return (contribution, chunkContext) -> {
             StepExecution stepExecution = contribution.getStepExecution();
-            JobParameters jobParameters = stepExecution.getJobParameters();
+//            JobParameters jobParameters = stepExecution.getJobParameters();
 
-            String value = jobParameters.getString("chunkSize", "10");
+//            String value = jobParameters.getString("chunkSize", "10");
             int chunkSize = StringUtils.isNotEmpty(value) ? Integer.parseInt(value) : 10;
             int fromIndex = stepExecution.getReadCount();
             int toIndex = fromIndex + chunkSize;
